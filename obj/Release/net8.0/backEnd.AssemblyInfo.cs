670be529979a56b10e3c4362e48a2b00f289940e
DiegxR <juandiegorojasarredondo@gmail.com>
2024-12-04 16:53:42 -0500

    fix build deploy

--- obj/Release/net8.0/backEnd.AssemblyInfo.cs
+++ obj/Release/net8.0/backEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdea2130d103b9856991c1d8c460c034bf9a16b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc29f710f4cfc1f3a561cddb2d6af21c40d45e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
